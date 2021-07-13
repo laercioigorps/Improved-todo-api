80313644ae445356380dbb71001eb7050dc38d74
laercio <laercioigorps@gmail.com>
2021-07-13 19:35:01 -0300

    updating models and tests

--- needs/models.py
+++ needs/models.py
@@ -13,8 +13,11 @@ class Goal(models.Model):
 	name = models.CharField(max_length=30)
 	description = models.CharField(max_length=80, default='')
 	endDate = models.DateField(default=date.today())
+	need = models.ForeignKey(Need, on_delete=models.CASCADE)
 
 class Step(models.Model):
 	name = models.CharField(max_length=30)
 	description = models.CharField(max_length=80, default='')
 	completed = models.BooleanField(default=False)
+	goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+
--- needs/tests.py
+++ needs/tests.py
@@ -60,8 +60,47 @@ class NeedBasicModelTests(TestCase):
 		self.assertEqual(count, 2)
 
 class StepModelTest(TestCase):
+
+	def setUp(self):
+
+		self.goal1 = Goal.objects.create(name='goal1')
+		self.step1 = Step.objects.create(name='first step', description='I like it',
+		 completed=False, goal=self.goal1)
+		self.step2 = Step.objects.create(name='second step', description='I kind of like it',
+		 completed=False, goal=self.goal1)
+		self.step3 = Step.objects.create(name='third step', description='I dont know if I like it',
+		 completed=False, goal=self.goal1)
+
 	def test_step_creation(self):
-		Step.objects.create(name='first step', description='i like it', completed=False)
+		count = Step.objects.all().count()
+		self.assertEqual(count, 3)
+		Step.objects.create(name='first step', description='I like it will pass',
+		 completed=False, goal=self.goal1)
+		count = Step.objects.all().count()
+		self.assertEqual(count, 4)
+
+	def test_need_retrieve_and_update(self):
+		step = Step.objects.get(name='first step')
+		step.name = 'firstNameUpdate'
+		step.description = 'firstDescriptionUpdate'
+		step.completed = True
+		step.save()
+
+		stepUpdated = Step.objects.get(name='firstNameUpdate')
+
+		self.assertEqual(stepUpdated.name, 'firstNameUpdate')
+		self.assertEqual(stepUpdated.description, 'firstDescriptionUpdate')
+		self.assertIs(stepUpdated.completed, True )
+
+	def test_step_deletion(self):
+		count = Step.objects.all().count()
+		self.assertEqual(count, 3)
+		step = Step.objects.get(name='first step')
+		step.delete()
+		count = Step.objects.all().count()
+		self.assertEqual(count, 2)
+
+
 
 
 class GoalModelTest(TestCase):
@@ -71,16 +110,22 @@ class GoalModelTest(TestCase):
 		# self.user2 = User.objects.create_user('root2','email2@exemple.com','root')
 		# self.user3 = User.objects.create_user('root3','email2@exemple.com','root')
 
-		Need.objects.create(name='mind', description='a need we have', user=self.user1)
-		Need.objects.create(name='body', description='a need we have', user=self.user1)
-		Need.objects.create(name='financial', description='a need we have', user=self.user1)
+		self.need1 = Need.objects.create(name='mind', description='a need we have', user=self.user1)
+		self.need2 = Need.objects.create(name='body', description='a need we have', user=self.user1)
+		self.need3 = Need.objects.create(name='financial', description='a need we have', user=self.user1)
 
-		Goal.objects.create(name="teste")
+		Goal.objects.create(name="teste", need= self.need1)
+		Goal.objects.create(name="goal2", need= self.need1)
+		Goal.objects.create(name="goal3", need= self.need2)
 
 	def test_goal_create(self):
+		count = Goal.objects.all().count()
+		self.assertEqual(count, 3)
 		date = datetime.date.today()
-		Goal.objects.create(name="teste2", description='description teste', endDate=date)
+		Goal.objects.create(name="teste2", description='description teste', endDate=date, need=self.need3)
 		goal = Goal.objects.get(name='teste2')
+		count = Goal.objects.all().count()
+		self.assertEqual(count, 4)
 		self.assertEqual(goal.name, 'teste2')
 		self.assertEqual(goal.description, 'description teste')
 		self.assertEqual(goal.endDate, date)
