b97720dc805a1bec8aa15ad26c73969ad70a4c00
laercio <laercioigorps@gmail.com>
2021-07-16 06:24:40 -0300

    urls path updated to use views created: need_list_view, need_detail_view, goal_list_view(incompleted)

---
+++ needs/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from needs import views
+
+urlpatterns = [
+    path('need/', views.need_list_view),
+    path('need/<int:pk>/', views.need_detail_view),
+    path('goal/', views.goal_list_view)
+]
--- needs/views.py
+++ needs/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from .models import Need
+from .serializers import NeedSerializer, GoalSerializer
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
+
+def need_list_view(request):
+
+	if request.method == 'GET':
+		needs = Need.objects.all()
+		serializer = NeedSerializer(needs, many=True) 
+		return JsonResponse(serializer.data, safe=False)
+	if request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = NeedSerializer(data=data)
+		if(serializer.is_valid()):
+			serializer.save(user=request.user)
+			return JsonResponse(serializer.data, status=201)
+		else:
+			return JsonResponse(serializer.errors, status=400)
+
+def need_detail_view(request, pk):
+	try:
+		need = Need.objects.get(pk=pk)
+	except Need.DoesNotExist:
+		return HttpResponse(status=404)
+
+	if request.method == 'GET':
+		serializer = NeedSerializer(need)
+		return JsonResponse(serializer.data)
+
+	if request.method == 'PUT': #study
+		data = JSONParser().parse(request)
+		serializer = NeedSerializer(need, data = data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors, status=400)
+	if request.method == 'DELETE':
+		need.delete()
+		return HttpResponse(status=204)
+
+
+def goal_list_view(request):
+
+	# if request.method == 'GET':
+	# 	needs = Need.objects.all()
+	# 	serializer = NeedSerializer(needs, many= True)
+	# 	return JsonResponse(serializer.data, safe=False)
+
+	if request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = GoalSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.errors)
+
+
