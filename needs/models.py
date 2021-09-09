8937c23410e61134a23e04a9f7a5cce46aa57c6e
laercio <laercioigorps@gmail.com>
2021-09-09 06:04:50 -0300

    models update

--- needs/models.py
+++ needs/models.py
@@ -5,39 +5,42 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Need(models.Model):
-	name = models.CharField(max_length=30)
-	description = models.CharField(max_length=80, default='')
-	user = models.ForeignKey(User, on_delete =models.CASCADE)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=80, default='')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Goal(models.Model):
-	name = models.CharField(max_length=30)
-	description = models.CharField(max_length=80, default='')
-	endDate = models.DateField(auto_now_add=False, null=True)
-	need = models.ForeignKey(Need, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=80, default='')
+    endDate = models.DateField(auto_now_add=False, null=True)
+    need = models.ForeignKey(Need, on_delete=models.CASCADE)
+
 
 class Step(models.Model):
-	name = models.CharField(max_length=30)
-	description = models.CharField(max_length=80, default='')
-	completed = models.BooleanField(default=False)
-	goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=80, default='')
+    completed = models.BooleanField(default=False)
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+
 
 class Iteration(models.Model):
-	number = models.IntegerField(default = 0)
-	completed = models.BooleanField(default=False)
-	date = models.DateField(auto_now_add=False, null=True)
-	owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    number = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+    date = models.DateField(auto_now_add=False, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
 
 class Delivery(models.Model):
-	name = models.CharField(max_length=30)
-	description = models.CharField(max_length=100)
-	step = models.ForeignKey(Step, on_delete=models.CASCADE, null=True)#may be changed
-	iteration = models.ForeignKey(Iteration, on_delete=models.CASCADE, null = True)
-	completed = models.BooleanField(default=False)
-
-
-
-
-
-
-
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    step = models.ForeignKey(
+        Step, on_delete=models.CASCADE, null=True)  # may be changed
+    iteration = models.ForeignKey(
+        Iteration, on_delete=models.CASCADE, null=True)
+    completed = models.BooleanField(default=False)
