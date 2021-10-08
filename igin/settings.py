5d530de569fcac75b2afc681038bef556c295446
laercio <laercioigorps@gmail.com>
2021-10-08 05:20:01 -0300

    fixed exposed secret key value by loading  from env variable if defined or use the default

--- igin/settings.py
+++ igin/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o5okhw=xdwl!&s-jjf67u@#7(kqoe)3i5lsq=mi0e&(f$y+#jw'
+# SECRET_KEY = 'django-insecure-o5okhw=xdwl!&s-jjf67u@#7(kqoe)3i5lsq=mi0e&(f$y+#jw'
+
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'django-insecure-o5okhw=xdwl!&s-jjf67u@#7(kqoe)3i5lsq=mi0e&(f$y+#jw')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -152,7 +155,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#provisorio
+# provisorio
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
