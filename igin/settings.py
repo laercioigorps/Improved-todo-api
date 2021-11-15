1554a4d5fad7a68b77ee2b2bbfcd4eca88149f61
laercio <laercioigorps@gmail.com>
2021-11-15 15:37:25 -0300

    add 'iginapp.herokuapp.com to allowed host

--- igin/settings.py
+++ igin/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iginapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
